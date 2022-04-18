0f6a29773f4899a378a86d5fb015d1d040173784
Craig McGowan <craigmcgowan15@gmail.com>
2022-04-18 12:34:41 -0400

    refactors index.js into files

---
+++ Schemas/index.js
@@ -0,0 +1,40 @@
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    getAllUsers: {
+      type: new GraphQLList(UserType),
+      args: { id: { type: GraphQLInt } },
+      resolve(parentValue, args) {
+        return userData;
+      },
+    },
+  },
+});
+
+const RootMutation = new GraphQLObjectType({
+  name: "Mutation",
+  fields: {
+    createUser: {
+      type: UserType,
+      args: {
+        firstName: { type: GraphQLString },
+        lastName: { type: GraphQLString },
+        email: { type: GraphQLString },
+        password: { type: GraphQLString },
+      },
+      resolve(parentValue, args) {
+        userData.push({
+          id: userData.length + 1,
+          firstName: args.firstName,
+          lastName: args.lastName,
+          email: args.email,
+          password: args.password,
+        });
+        return args;
+      },
+    },
+  },
+});
+
+const schema = new GraphQLSchema({ query: RootQuery, mutation: RootMutation });
+
